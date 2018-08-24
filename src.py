9eeb9adb29d862e34abcbee9a77a711568b00b4e
Vlad Lazar <vladlazar@outlook.com>
2018-08-24 10:30:47 +0100

    unique load data

--- src.py
+++ src.py
@@ -17,22 +17,15 @@ def load_data():
     train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
     f.close()
 
-    return train_set, valid_set, test_set
-
-
-def load_data_wrapper():
-
-    training_data, validation_data, test_data = load_data()
-
-    training_inputs = [np.reshape(x, (784, 1)) for x in training_data[0]]
-    training_results = [vectorized_result(y) for y in training_data[1]]
+    training_inputs = [np.reshape(x, (784, 1)) for x in train_set[0]]
+    training_results = [vectorized_result(y) for y in train_set[1]]
     training_data = zip(training_inputs, training_results)
 
-    validation_inputs = [np.reshape(x, (784, 1)) for x in validation_data[0]]
-    validation_data = zip(validation_inputs, validation_data[1])
+    validation_inputs = [np.reshape(x, (784, 1)) for x in valid_set[0]]
+    validation_data = zip(validation_inputs, valid_set[1])
 
-    test_inputs = [np.reshape(x, (784, 1)) for x in test_data[0]]
-    test_data = zip(test_inputs, test_data[1])
+    test_inputs = [np.reshape(x, (784, 1)) for x in test_set[0]]
+    test_data = zip(test_inputs, test_set[1])
 
     return training_data, validation_data, test_data
 
